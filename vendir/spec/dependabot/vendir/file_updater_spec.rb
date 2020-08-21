2324a0b54f321aca9f2caa8874e52458cce5f005
Nick Rohn <nrohn@pivotal.io>
2020-08-21 09:55:25 -0700

    wip

--- vendir/spec/dependabot/vendir/file_updater_spec.rb
+++ vendir/spec/dependabot/vendir/file_updater_spec.rb
@@ -7,5 +7,68 @@ require "dependabot/vendir/file_updater"
 require_common_spec "file_updaters/shared_examples_for_file_updaters"
 
 RSpec.describe Dependabot::Vendir::FileUpdater do
-  it_behaves_like "a dependency file updater"
+  # it_behaves_like "a dependency file updater"
+  let(:updater) do
+    described_class.new(
+        dependencies: dependencies,
+        dependency_files: dependency_files,
+        credentials: credentials,
+    )
+  end
+
+  let(:credentials) do
+    [{
+         "type" => "git_source",
+         "host" => "github.com",
+         "username" => "x-access-token",
+         "password" => "token"
+     }]
+  end
+  let(:dependency_files) { [] }
+  let(:dependencies) { [dependency] }
+  let(:dependency) do
+    Dependency.new(
+        name: "config/_ytt_lib/github.com/cloudfoundry/cf-k8s-networking",
+        version: "4153bbdbaf3e8d3d681a84a112f3c9cc10129ff3",
+        requirements: [{
+                           requirement: "v0.0.6",
+                           groups: [],
+                           source: {
+                               type: "git",
+                               branch: "v0.0.6",
+                               url: "https://github.com/cloudfoundry/cf-k8s-networking",
+                           },
+                           file: vendir_yml.path,
+                       }],
+        package_manager: "vendir"
+    )
+  end
+
+
+  describe "#updated_dependency_files" do
+    subject(:updated_files) { updater.updated_dependency_files }
+    # let(:process_status) { double }
+    # let(:command) { /kiln update-release --name uaa --version #{current_version} -kf .*\/Kilnfile -rd .*\/ -vr aws_access_key_id=foo -vr aws_secret_access_key=foo/ }
+
+    before do
+      # allow(process_status).to receive(:success?).and_return true
+      # allow(Open3).to receive(:capture3).and_return('', '', process_status)
+    end
+
+    it "returns DependencyFile objects" do
+      # updated_files.each { |f| expect(f).to be_a(Dependabot::DependencyFile) }
+      expect(true).to eq(true)
+    end
+
+    its(:length) { is_expected.to eq(1) }
+
+    it "has updated dependency" do
+      # expect(updated_files[0].content).to eq(updated_lockfile_body)
+    end
+
+    it "calls kiln to update the dependency" do
+      # updated_files
+      # expect(Open3).to have_received(:capture3).with(command)
+    end
+  end
 end
